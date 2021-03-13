4a55a4b0c6f6bdc795b5148e96d2a98482f4408d
Ricardo Salvatorelli <rsca4321@gmail.com>
2021-03-13 13:00:14 -0400

    posible fix a Navbar no cargando

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,6 +11,9 @@
 
 import Navbar from './components/Navbar';
 
+sessionStorage.setItem('SERVER_PORT', '5000/api');
+sessionStorage.setItem('SERVER_URL', 'http://10.0.0.100');
+
 export default {
   name: 'App',
 
